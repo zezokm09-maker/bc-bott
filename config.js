01f1a3a8eb9b2df20f6b0b7b6a6cf8654bb748c7
zezokm09-maker <zezokm09@gmail.com>
2026-08-19 04:26:21 +0300

    Update token in config.js
    Updated the token value in config.js.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-token:"",
+token:"MTUzOTMzMzc3NjMzOTg5ODQ0OQ.GDIGZ_.06KJ7LZZqEemFV9Uh2lzvpVTX1EL7aek9H4R5A",
 statusname: "", // اسم حالة البوت 
 owners: ["1294613089525825567"], // الاشخاص اللي بأمكانهم استعمال البوت
 Prefix: "!", // خله
