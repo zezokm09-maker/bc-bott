85fa6311b2ccec17ce413bd718583102656b8128
zezokm09-maker <zezokm09@gmail.com>
2026-08-19 04:38:03 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-token:"",
-statusname: "", // اسم حالة البوت 
-owners: ["1294613089525825567"], // الاشخاص اللي بأمكانهم استعمال البوت
-Prefix: "!", // خله
-Time: 5000, // خله
+    token:"MTUzOTMzMzc3NjMzOTg5ODQ0OQ.GDIGZ_.06KJ7LZZqEemFV9Uh2lzvpVTX1EL7aek9H4R5A",
+    statusname: "",
+    owners: ["1294613089525825567"],
+    Prefix: "!",
+    Time: 5000
 }
 
 // امر استعمال البوت
-// !bc 
+// !bc
--- main.js
+++ main.js
@@ -676,3 +676,4 @@ client.on("message", async message => {
 });
 
 client.login(config.token);
+client.login(process.env.TOKEN);
